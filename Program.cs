2eb31cebdb741086aa0188e4803e2f1f4488d696
botea <botea@IULIA>
2022-11-30 13:17:53 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+public static class Globals
+{
+	internal static void Main()
+	{
+		Console.Write("Cati ani bisecti sunt in intervalul dintre ani:");
+		Console.Write("\n");
+		int y1;
+		int y2;
+		int bis = 0;
+
+		Console.Write("Y1 = ");
+		y1 = Convert.ToInt32(Console.ReadLine());
+		Console.Write("Y2 = ");
+		y2 = Convert.ToInt32(Console.ReadLine());
+		y1++;
+		while (y1 < y2)
+		{
+
+			if (y1 % 4 == 0)
+			{
+				bis++;
+			}
+			if (y1 % 100 == 0)
+			{
+				bis--;
+			}
+			if (y1 % 400 == 0)
+			{
+				bis++;
+			}
+
+			y1++;
+
+		}
+		Console.Write("In acest interval sunt - ");
+		Console.Write(bis);
+		Console.Write("  ani bisecti");
+
+
+
+	}
+}
+
+
